64d974e2d5e24b3c812a06a3c87a6cbc881620da
kisyamane <yuraserb05@gmail.com>
2026-03-15 18:17:25 +0300

    habr parser

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,8 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-import "reflect-metadata";
 import { healthRouter } from "./routers/health";
+import { habrRouter } from "./routers/habr";
 import { wikipediaRouter } from "./routers/wikipedia";
-import { authRouter } from "./routers/auth";
 
 dotenv.config();
 
@@ -12,8 +11,8 @@ const PORT: number = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 app.use("/health", healthRouter);
+app.use("/habr", habrRouter);
 app.use("/wikipedia", wikipediaRouter);
-app.use("/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
---
+++ backend/src/integrations/habr/index.ts
@@ -0,0 +1,10 @@
+export { HabrApiClient } from "./client";
+export type { HabrParsedArticle } from "./basic.types";
+export type {
+  HabrPublication,
+  HabrPublicationHub,
+  HabrPublicationLeadData,
+  HabrPublicationStatistics,
+} from "./entity.types";
+export type { HabrSearchArticlesRequest, HabrQueryParams } from "./request.types";
+export type { HabrSearchArticlesResponse } from "./response.types";
