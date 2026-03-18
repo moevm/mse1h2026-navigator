19e4e517645a6a286920c84cce48f4884fa78083
Egor Anakhin <eanahin8v@mail.ru>
2026-03-18 15:34:35 +0300

    feat: skills relations normalizer service

--- graph-data-service/src/main.py
+++ graph-data-service/src/main.py
@@ -7,6 +7,7 @@ from fastapi import FastAPI
 from .services.skills_finder import SkillsFinder
 from .services.skills_normalizer import SkillsNormalizer
 from .services.skills_relation_finder import SkillsRelationFinder
+from .services.skills_relations_normalizer import SkillRelationNormalizer
 
 dotenv.load_dotenv()
 
@@ -22,10 +23,12 @@ def read_root():
 def get_skill_graph(job_title: str):
     print("WORKING")
     safe_job_title = job_title.replace(" ", "_").lower()
-    file_path = f"normalized_skills_{safe_job_title}.json"
 
-    if os.path.exists(file_path):
-        with open(file_path, "r", encoding="utf-8") as f:
+    skills_file_path = f"normalized_skills_{safe_job_title}.json"
+    relations_file_path = f"skill_relations_{safe_job_title}.json"
+
+    if os.path.exists(skills_file_path):
+        with open(skills_file_path, "r", encoding="utf-8") as f:
             normalized_skills = json.load(f)
     else:
         _skills_finder = SkillsFinder(job_title)
@@ -34,10 +37,22 @@ def get_skill_graph(job_title: str):
         skills_normalizer = SkillsNormalizer(skills_list, job_title)
         normalized_skills = skills_normalizer.get_normalized_skills()
 
-        with open(file_path, "w", encoding="utf-8") as f:
+        with open(skills_file_path, "w", encoding="utf-8") as f:
             json.dump(normalized_skills, f, ensure_ascii=False, indent=4)
 
-    skills_relation_finder = SkillsRelationFinder(job_title, normalized_skills)
-    skill_relations = skills_relation_finder.find_skill_relations()
+    if os.path.exists(relations_file_path):
+        with open(relations_file_path, "r", encoding="utf-8") as f:
+            skill_relations = json.load(f)
+        print(f"Loaded existing relations from {relations_file_path}")
+    else:
+        skills_relation_finder = SkillsRelationFinder(job_title, normalized_skills)
+        skill_relations = skills_relation_finder.find_skill_relations()
 
-    return {"response": skill_relations}
+        with open(relations_file_path, "w", encoding="utf-8") as f:
+            json.dump(skill_relations, f, ensure_ascii=False, indent=4)
+        print(f"Saved new relations to {relations_file_path}")
+
+    skills_relation_normalizer = SkillRelationNormalizer(job_title, skill_relations)
+    normalized_relations = skills_relation_normalizer.get_normalized_relations()
+
+    return normalized_relations
